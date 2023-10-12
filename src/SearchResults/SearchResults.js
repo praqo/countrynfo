c8154f7e30b373e1ad61f2f8b33fb264080852b6
praqo <142553687+praqo@users.noreply.github.com>
2023-10-11 21:56:08 -0700

    add states for loading, fetched data and use query hook to get url parameter

--- src/SearchResults/SearchResults.js
+++ src/SearchResults/SearchResults.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useQuery } from "../shared/useQuery";
+const url = "https://restcountries.eu/rest/v2/name/";
 
 export function SearchResults() {
-  return (
-    <div>
-      <h2>SearchResults</h2>
-    </div>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchData, setSearchData] = useState([]);
+  const query = useQuery();
+
+  const searchQuery = query.get("q");
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url + searchQuery);
+      const data = await response.json();
+      setIsLoading(false);
+      setSearchData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log(searchData);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h3>...Loading</h3>
+      </div>
+    );
+  }
+
+  if (searchData.length) {
+    return (
+      <div>
+        {searchData.map((item) => {
+          return <h3 key={item.alpha2Code}>{item.name}</h3>;
+        })}
+      </div>
+    );
+  }
+
+  return <h3>No Results</h3>;
 }
