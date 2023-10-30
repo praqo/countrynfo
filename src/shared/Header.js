aa27352311826a88831c60b36e870763b17130ef
praqo <142553687+praqo@users.noreply.github.com>
2023-10-29 21:33:08 -0700

    add useLocation hook to detect current pathname

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,6 +1,13 @@
 import Search from "./Search";
+import { useLocation } from "react-router-dom";
 
 function Header() {
+  const location = useLocation();
+
+  if (location.pathname === "/") {
+    return null;
+  }
+
   return (
     <header className="main-header">
       <a href="/" className="header-logo" title="home">
